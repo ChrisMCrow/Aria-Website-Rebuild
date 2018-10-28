3bb11c0ea5da2124d15de25cebf9f9d80379d7d4
Chris Crow <chrismcrow@gmail.com>
2018-10-28 13:24:24 -0700

    begin footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeContentComponent } from './home-content/home-content.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeContentComponent
+    HomeContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
