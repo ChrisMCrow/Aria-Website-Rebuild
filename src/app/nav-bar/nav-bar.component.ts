665bd5af29b295814504156e8d1b379b2da3f79d
Chris Crow <chrismcrow@gmail.com>
2018-11-02 10:25:32 -0700

    add dropdown form to book room button in nav.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,7 +10,20 @@ export class NavBarComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  submitForm(checkIn: string, checkOut: string, guests: string): void {
+    console.log(guests);
+    window.location.replace(`https://www.aria.com/en/booking/room-booking.html#/rooms?numGuests=${guests}&arrive=${checkIn}&depart=${checkOut}`);
   }
 
+  
+  ngOnInit() {
+  }
+  
 }
+
+$(document).ready(function() {
+  $('.dropdown-menu option, .dropdown-menu select').click(function(event) {
+    event.stopPropagation();
+  });
+});
+    
