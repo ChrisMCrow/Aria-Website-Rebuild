814d9ddbd2717d707ef9123fc145249d90e74ae0
Chris Crow <chrismcrow@gmail.com>
2018-11-09 12:22:32 -0800

    add api keys to project

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export const masterFirebaseConfig = {
+  apiKey: "AIzaSyC3dq2206pqkER-ty-_bHHvvzqbSFIvzCg",
+  authDomain: "ariawebsiteclone.firebaseapp.com",
+  databaseURL: "https://ariawebsiteclone.firebaseio.com",
+  projectId: "ariawebsiteclone",
+  storageBucket: "ariawebsiteclone.appspot.com",
+  messagingSenderId: "957646700867"
+};
